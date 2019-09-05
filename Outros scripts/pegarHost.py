cd11e9ba45d02afa0291edc1792eedbbd348737e
Rafael Silva <rafael.filgue@gmail.com>
2019-09-05 15:14:51 -0300

    Ajustes

--- Outros scripts/pegarHost.py
+++ Outros scripts/pegarHost.py
@@ -7,13 +7,13 @@ from datetime import datetime
 import time
 
 
-zapi = ZabbixAPI("https://sm-fe01.webmonitor.global")
+zapi = ZabbixAPI("url_zabbix")
 # Disable SSL certificate verification
 zapi.session.verify = False
 # Specify a timeout (in seconds)
 zapi.timeout = 10.1
 # informacoes de acesso
-zapi.login("api.linx", "Linx#zabbix@19")
+zapi.login("user_zabbix", "senha_user")
 # mostra versao do zabbix
 print("Connected to Zabbix API Version %s" % zapi.api_version())
 
